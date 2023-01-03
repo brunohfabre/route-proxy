e00417c3771a1f9aea406f9888f99d17244adc27
Bruno Fabre <bruno.hfabre@gmail.com>
2023-01-03 14:12:16 -0300

    first commit

---
+++ app-02/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div>
+      <h1>App 02</h1>
+    </div>
+  )
+}
---
+++ root/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div>
+      <h1>Root</h1>
+    </div>
+  )
+}
