b71fd1b9f7f4703d0ed5373a5431e727485b2d18
Bruno Fabre <bruno.hfabre@gmail.com>
2023-01-03 17:27:33 -0300

    add navigate button to app-01

--- app-01/src/pages/Home.tsx
+++ app-01/src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
 export function Home() {
+  const navigate = useNavigate()
+
   return (
     <div>
       <h1>app 01 - home</h1>
+
+      <button type='button' onClick={() => navigate('/test')}>go to test</button>
     </div>
   )
 }
