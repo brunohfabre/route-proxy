4e3a88292080045681e4331318b4d18bd3469560
Bruno Fabre <bruno.hfabre@gmail.com>
2023-01-05 11:56:07 -0300

    fix

--- app-01/src/pages/Test.tsx
+++ app-01/src/pages/Test.tsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
 export function Test() {
+  const navigate = useNavigate()
+
   return (
     <div>
       <h1>App 01 - test</h1>
+
+      <button type='button' onClick={() => window.location.href = '/app-02'}>go to app-02</button>
     </div>
   )
 }
